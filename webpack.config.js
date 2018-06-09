f2653f48087f595a302281605151c8e6b5d4758b
keidrun <kssk96@gmail.com>
2018-06-09 02:35:59 -0700

    Adjust css url because of custom domain

--- webpack.config.js
+++ webpack.config.js
@@ -8,105 +8,65 @@ const PATH = {
   INDEX: {
     JS: './src/components/index.jsx',
     CSS: './src/styles/index.scss',
-    HTML: './src/index.html',
+    HTML: './src/index.html'
   },
   DIST: path.join(__dirname, 'public'),
-  IMG: {
-    IN: 'src/assets',
-    OUT: 'assets',
-  },
+  IMG: { IN: 'src/assets', OUT: 'assets' }
 };
-const VENDOR_LIBS = ['react', 'react-dom', 'bootstrap', 'jquery', 'popper.js', 'tether'];
+const VENDOR_LIBS = [ 'react', 'react-dom', 'bootstrap', 'jquery', 'popper.js', 'tether' ];
 
 module.exports = {
   entry: {
-    bundle: [PATH.INDEX.JS, PATH.INDEX.CSS, PATH.INDEX.HTML],
-    vendor: VENDOR_LIBS,
+    bundle: [ PATH.INDEX.JS, PATH.INDEX.CSS, PATH.INDEX.HTML ],
+    vendor: VENDOR_LIBS
   },
-  output: {
-    path: PATH.DIST,
-    filename: '[name].[hash].js',
-  },
+  output: { path: PATH.DIST, filename: '[name].[hash].js' },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: ['remove-flow-types-loader'],
+        use: [ 'remove-flow-types-loader' ],
         enforce: 'pre',
-        include: path.join(__dirname, 'src'),
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.(js|jsx)$/,
-        use: ['eslint-loader'],
+        use: [ 'eslint-loader' ],
         enforce: 'pre',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: [
-            'css-loader',
-            {
-              loader: 'css-url-loader',
-              query: {
-                from: '/assets/',
-                to: '/portfolio/assets/',
-                env: process.env.NODE_ENV,
-              },
-            },
-            'postcss-loader',
-            'sass-loader',
-          ],
-        }),
+          use: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
+        })
       },
       {
         test: /\.(jpe?g|png|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-        ],
+        use: [ { loader: 'url-loader', options: { limit: 8192 } } ]
       },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-      },
-    ],
+      { test: /\.html$/, use: 'html-loader' }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      images: path.join(__dirname, 'public/assets'),
-    },
+    extensions: [ '.js', '.jsx' ],
+    alias: { images: path.join(__dirname, 'public/assets') }
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest'],
-    }),
-    new HtmlWebpackPlugin({
-      template: PATH.INDEX.HTML,
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
+    new webpack.optimize.CommonsChunkPlugin({ names: [ 'vendor', 'manifest' ] }),
+    new HtmlWebpackPlugin({ template: PATH.INDEX.HTML }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default'],
+      Popper: [ 'popper.js', 'default' ]
     }),
     new ExtractTextPlugin('bundle.[hash].css'),
-    new CopyWebpackPlugin([{ from: PATH.IMG.IN, to: PATH.IMG.OUT }]),
-    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin([ { from: PATH.IMG.IN, to: PATH.IMG.OUT } ]),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devtool: 'source-map',
   devServer: {
@@ -115,10 +75,6 @@ module.exports = {
     port: 8080,
     inline: true,
     historyApiFallback: true,
-    stats: {
-      version: false,
-      hash: false,
-      chunkModules: false,
-    },
-  },
+    stats: { version: false, hash: false, chunkModules: false }
+  }
 };
