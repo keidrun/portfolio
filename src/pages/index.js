4b580049097e158b349441c571574a7d141b81ab
keidrun <kssk96@gmail.com>
2019-08-25 02:28:10 +0900

    Add favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import favicon from '../../static/favicon.ico'
 
 import Layout from '../components/layout'
 import Portfolio from '../components/portfolio'
@@ -15,6 +16,7 @@ const IndexPage = () => (
     <Helmet>
       <meta charSet="utf-8" />
       <meta name="robots" content="noindex,nofollow" />
+      <link rel="shortcut icon" href={favicon} />
       <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,300,300i,500,500i" rel="stylesheet" />
       <link href="https://keisukesasaki.com/assets/fonts/et-line-font/et-line-font.css" rel="stylesheet" />
       <body data-spy="scroll" data-target=".navbar" data-offset="50" />
